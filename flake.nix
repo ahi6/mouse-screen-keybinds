94f03c06fba2be1b87027c1ff030c8b25644a831
ahi6 <60984726+ahi6@users.noreply.github.com>
2026-09-19 17:47:01 +0200

    nix: OpenGL -> Vulkan
    the OpenGL backend no longer work correctly
    Vulkan does
    libGL is still needed for lwjgl

--- flake.nix
+++ flake.nix
@@ -8,16 +8,16 @@
     devShells.${system}.default = pkgs.mkShell {
       buildInputs = [ pkgs.jdk25 ];
       LD_LIBRARY_PATH = pkgs.lib.makeLibraryPath [
+        pkgs.vulkan-loader
         pkgs.libGL
-        pkgs.glfw
         pkgs.openal
         pkgs.libpulseaudio
         pkgs.udev
-        pkgs.xorg.libX11
-        pkgs.xorg.libXcursor
-        pkgs.xorg.libXrandr
-        pkgs.xorg.libXi
-        pkgs.xorg.libXext
+        pkgs.libX11
+        pkgs.libXcursor
+        pkgs.libXrandr
+        pkgs.libXi
+        pkgs.libXext
       ];
     };
   };
